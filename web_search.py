5979d022f80f4a746c8b9687506bfd9140610cda
Priti Yadav <pyadav0323@gmail.com>
2025-09-30 10:51:57 -0400

    commiting changes

--- web_search.py
+++ web_search.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
-from serpapi import GoogleSearch
-from models import Product
+from serpapi.google_search import GoogleSearch
+from langchain_core.tools import tool
 
 # Load .env file
 load_dotenv()
@@ -10,13 +10,24 @@ load_dotenv()
 api_key = os.getenv("SERP_API")
 
 #  Function to search for a product and return results usinf SerpAPI
-def search_product(product_searched, num_results):
-    
+@tool
+def web_search_tool(query: str) -> str:
+    """
+    Perform a Google search using SerpAPI.
+
+    Args:
+        query (str): The search query.
+
+    Returns:
+        str: A formatted list of top search results (title and link).
+    """
+
+    print(f"Searching for: {query}")
     params = {
     "engine": "google",
-    "q": product_searched,
+    "q": query,
     "api_key": api_key,
-    "num": num_results
+    "num": 3
     }
 
     search = GoogleSearch(params)
@@ -24,22 +35,37 @@ def search_product(product_searched, num_results):
     #print(results) #immersive_products
     organic_results = results["organic_results"]
     #prod_results = results["immersive_products"]
-    products = []
-    for r in organic_results[:num_results]:
-        try:
-            # Map API fields to Pydantic model fields
-            product = Product(
-                product_name=r.get("title"),
-                price=float(r.get("price", 0)),  # fallback 0 if missing
-                link=r.get("link")
-            )
-            products.append(product)
-        except Exception as e:
-            print(f"Skipping invalid result: {r} - {e}")
-    return products
 
-if __name__ == "__main__":
+    output = []
+    for r in organic_results:
+        output.append(f"{r.get('title')} — {r.get('link')}")
+    
+    return "\n".join(output[:3])
+
+
+# for r in organic_results[:num_results]:
+#     try:
+#         # Map API fields to Pydantic model fields
+#         product = Product(
+#             product_name=r.get("title"),
+#             price=float(r.get("price", 0)),  # fallback 0 if missing
+#             link=r.get("link")
+#         )
+#         products.append(product)
+#     except Exception as e:
+#         print(f"Skipping invalid result: {r} - {e}")
+# return products
+
+
+def readFile(sfilename):
+    with open(sfilename, 'r') as f1:
+        data = f1.read()
+        print(data)
+    return data
+
+#if __name__ == "__main__":
     # search for "iPhone 16 Pro" and get top 2 results
-    products = search_product("iPhone 16 Pro", 2)
-    for p in products:
-        print(p.json()) 
+    #products = search_product("iPhone 16 Pro", 2)
+    #for p in products:
+    #    print(p.json()) 
+    #readFile('test.txt')   
